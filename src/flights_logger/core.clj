40afe5ca3fd884f12995ba87a17b82e0f56281d9
Max Fitton <maxfitton@gmail.com>
2016-04-01 00:30:44 -0700

    Added my first test for generating the API call body.

--- src/flights_logger/core.clj
+++ src/flights_logger/core.clj
@@ -1,16 +1,41 @@
 (ns flights-logger.core
+
   (:gen-class)
   (:require [clojure.data.json :as json] 
             [clj-http.client :as client]))
-(def num-solutions
+
+(declare process-res)
+(declare get-results)
+(declare qpx-express-request-body)
+(declare flight-specifications)
+
+(def num-itineraries 25)
+(def default-passenger-args
+  { :adultCount 1
+    :infantInLapCount 0
+    :infantInSeatCount 0
+    :childCount 0
+    :seniorCount 0 })
+(def qpx-express-key "IzaSyDRSEMJ2FgABK7D32HFixT22iPWyYL4bLwa")
+
+(def qpx-express-url "https://www.googleapis.com/qpxExpress/v1/trips/search")
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
+
+(defn process-res
+  "Processes the response from QPX flights database"
+  [res]
+  (json/read-str res))
+
 (defn qpx-express-request-body
+  "Creates the body of a QPX express request with given arguments."
   [origin dest date-out earliest-out latest-out 
-               date-back earliest-back earlist-out
+               date-back earliest-back latest-back
                max-price]
   "Makes a request to the engine with specified parameters"
   {:request 
@@ -18,11 +43,21 @@
           [(flight-specifications origin dest date-out earliest-out latest-out)
            (flight-specifications dest origin date-back earliest-back latest-back)]
        :passengers default-passenger-args
-       :solutions 25
-       :maxPrice (or max-price "USD400.00")
+       :solutions num-itineraries
+       :maxPrice (or max-price "USD500.00")
        :refundable false
       }
-)
+  })
+
+(defn get-results
+  "Performs a request for matching results from the QPX flights database"
+  [origin dest date-out earliest-out latest-out
+   date-back earliest-back latest-back max-price]
+  (client/post qpx-express-url (json/write-str 
+                                   (qpx-express-request-body origin dest 
+                                    date-out earliest-out latest-out
+                                    date-back earliest-back latest-back
+                                    max-price))))
 
 (defn flight-specifications
   "Returns the slice argument for a request to qpx express
@@ -30,22 +65,14 @@
   
   [origin dest date earliest latest]
   (let [shared { :origin origin
-                 :dest dest
+                 :destination dest
                  :date date
                  :preferredCabin "COACH"
                }]
-  (if (and (?nil earliest) (?nil latest))
+  (if (and (nil? earliest) (nil? latest))
         shared
         (merge shared {:permittedDepartureTime
                         {:earliestTime (or earliest "00:00")
                          :latestTime (or latest "24:00")}}))))
 
- 
 
-(def default-passenger-args
-  { :adultCount 1
-    :infantInLapCount 0
-    :infantInSeatCount 0
-    :childCount 0
-    :seniorCount 0 }
-)
--- test/flights_logger/core_test.clj
+++ test/flights_logger/core_test.clj
@@ -1,7 +1,31 @@
 (ns flights-logger.core-test
   (:require [clojure.test :refer :all]
-            [flights-logger.core :refer :all]))
+            [flights-logger.core :refer :all]
+            [clojure.pprint :as pprint]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def expected-query-body
+  {:request {:slice [{:origin "SFO"
+                     :destination "STL"
+                     :date "2016-04-01"
+                     :preferredCabin "COACH"}
+                     {:origin "STL"
+                      :destination "SFO"
+                      :date "2016-04-04"
+                      :preferredCabin "COACH"}]
+             :passengers {:adultCount 1
+                          :infantInLapCount 0
+                          :infantInSeatCount 0
+                          :childCount 0
+                          :seniorCount 0}
+             :solutions 25
+             :maxPrice "USD500.00"
+             :refundable false
+             }
+   }
+                      )
+
+(deftest correct-query-body
+  (testing "qpx-express-request-body creates a correct request"
+    (is (= expected-query-body (qpx-express-request-body
+                                 "SFO" "STL" "2016-04-01" nil nil 
+                                 "2016-04-04" nil nil "USD500.00")))))
